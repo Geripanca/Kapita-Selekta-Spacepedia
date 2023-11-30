ba2429d037ada8c7761e2074d71297d29a0e3d6e
Geripanca <geri@upi.edu>
2023-11-30 12:50:54 +0700

    first-commit

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,5 @@
 import css from "rollup-plugin-css-only";
 
 export default {
-  input: "src/entry.js",
-  output: {
-    dir: "dist",
-    format: "esm",
-  },
-  plugins: [
-    // ... plugin lainnya ...
-    css({ output: "bundle.css" }), // Menggunakan rollup-plugin-css-only
-  ],
+  plugins: [css({ output: "dist/bundle.css" })],
 };
